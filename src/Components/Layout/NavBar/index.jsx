0d9c7a61e4ca3c3a0f35eb4c04a84b2f46235a42
FrankiCode <jaceknauka@gmail.com>
2025-03-07 01:13:22 +0400

    navbar

--- src/Components/Layout/NavBar/index.jsx
+++ src/Components/Layout/NavBar/index.jsx
@@ -20,11 +20,16 @@ const NavBar = () => {
   return (
     <div className='relative flex justify-between items-center px-5 py-5 select-none duration-500'>
         {showBar ? (<LiaTimesSolid className=' md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}} />) : (<HiMiniBars3  className='md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}}/>)}
+
         <Link to="/"><h1 className='md:text-3xl lg:text-4xl font-bold text-[var(--mainColor)] font-serif cursor-pointer tracking-[5px] duration-500'>NavBar</h1></Link>
+
         <div className='flex gap-2.5 items-center duration-500'>
             <CiSearch className='md:text-xl lg:text-2xl text-[var(--mainColor)] cursor-pointer z-10 duration-500' onClick={() => {setShowSearch(!showSearch)}}/>
+
             <Link to="/login"><Button title='Login' className="lg:text-xl border border-[var(--mainColor)] text-[var(--mainColor)] px-[20px] rounded cursor-pointer hover:bg-[var(--backgound)] duration-600"/></Link>
+
             <HiOutlineShoppingBag className='text-2xl text-[var(--mainColor)] cursor-pointer duration-500'/> 
+            
             {showSearch && <div className='absolute right-[12%] duration-500'>
                 <input type="search" placeholder='search...' className='lg:w-70 border rounded border-[var(--mainColor)] placeholder:text-[var(--mainColor)] placeholder:text-[14px] text-[14px] px-[20px] py-[3px] outline-none text-[var(--mainColor)] duration-500'/>
             </div>}
