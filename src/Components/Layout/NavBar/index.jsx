bac601263495a6f82b62dbe0d7109acb3f3efa1f
FrankiCode <jaceknauka@gmail.com>
2025-03-07 13:20:55 +0400

    updated-- menu backgroung color, login backgroung color

--- src/Components/Layout/NavBar/index.jsx
+++ src/Components/Layout/NavBar/index.jsx
@@ -26,10 +26,10 @@ const NavBar = () => {
         <div className='flex gap-2.5 items-center duration-500'>
             <CiSearch className='md:text-xl lg:text-2xl text-[var(--mainColor)] cursor-pointer z-10 duration-500' onClick={() => {setShowSearch(!showSearch)}}/>
 
-            <Link to="/login"><Button title='Login' className="lg:text-xl border border-[var(--mainColor)] text-[var(--mainColor)] px-[20px] rounded cursor-pointer hover:bg-[var(--backgound)] duration-600"/></Link>
+            <Link to="/login"><Button title='Login' className="lg:text-xl border border-[var(--mainColor)] text-[var(--mainColor)] px-[20px] rounded cursor-pointer hover:bg-[var(--background)] duration-600"/></Link>
 
             <HiOutlineShoppingBag className='text-2xl text-[var(--mainColor)] cursor-pointer duration-500'/> 
-            
+
             {showSearch && <div className='absolute right-[12%] duration-500'>
                 <input type="search" placeholder='search...' className='lg:w-70 border rounded border-[var(--mainColor)] placeholder:text-[var(--mainColor)] placeholder:text-[14px] text-[14px] px-[20px] py-[3px] outline-none text-[var(--mainColor)] duration-500'/>
             </div>}
@@ -38,13 +38,15 @@ const NavBar = () => {
 
 
         { showBar && 
-        (<div className="flex flex-col absolute top-[150px] h-[calc(100vh-80px)] justify-top tracking-[4px] duration-500 all ease-in">
+        (<div className="absolute w-[400px] flex flex-col top-[80px] left-0 h-[calc(100vh-80px)] pt-10 tracking-[4px] pl-10 duration-500 all ease-in bg-[var(--background)]">
             {
-                NavBar.map(({id, title, href, icon}) => {
-                    return <NavLink to={href} key={id} className="text-xl text-[var(--mainColor)] font-thin hover:bg-[var(--backgound)] px-2.5 duration-600">{title}{icon}</NavLink>
+                NavBar.map(({id, title, href}) => {
+                    return <NavLink to={href} key={id} className="text-xl text-[var(--mainColor)] font-thin hover:bg-[var(--background)] pl-2.5 duration-600">{title}</NavLink>
                 })
             }
-            <NavLink to="/login" className="relative flex items-center text-xl text-[var(--mainColor)] bg-[var(--backgound)] font-thin px-2.5 mt-5">Login <CiLogin className='absolute right-10 bottom-[2px]'/></NavLink>
+            <div className='bg-[var(--mainColor)] w-[40%] mt-5 pl-2.5'>
+                <NavLink to="/login" className="relative flex items-center text-xl text-[var(--background)] font-thin">Login <CiLogin className='absolute left-20 bottom-[2px]'/></NavLink>
+            </div>
         </div>)}
     </div>
   )
