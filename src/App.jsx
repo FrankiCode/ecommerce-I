bad1b4379a85ed99641d9450f5a0b5b0a15bafb7
FrankiCode <jaceknauka@gmail.com>
2025-03-07 18:22:26 +0400

    added mx-auto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './Components/Layout/NavBar'
+import WebRoutes from './Components/WebRoutes'
 
 function App() {
 
 
   return (
-    <>
+    <div className='mx-auto'>
       <NavBar/>
-    </>
+      <WebRoutes/>
+    </div>
   )
 }
 
