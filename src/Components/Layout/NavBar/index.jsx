68a1b01e2f33f35dfbe5b17ad51380ac4cd4b7fe
FrankiCode <jaceknauka@gmail.com>
2025-03-07 15:10:05 +0400

    changed sticky menu & responsive menu &  background

--- src/Components/Layout/NavBar/index.jsx
+++ src/Components/Layout/NavBar/index.jsx
@@ -18,17 +18,17 @@ const NavBar = () => {
         {id:3, title: "Contact us", href: "/contact"},
     ]
   return (
-    <div className='relative flex justify-between items-center px-5 py-5 select-none duration-500'>
-        {showBar ? (<LiaTimesSolid className=' md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}} />) : (<HiMiniBars3  className='md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}}/>)}
+    <div className='sticky top-0 bg-[var(--background)] md:bg-transparent flex justify-between items-center px-5 py-5 select-none duration-500 z-1'>
+        {showBar ? (<LiaTimesSolid className='text-xl md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}} />) : (<HiMiniBars3  className='text-xl md:text-2xl lg:text-3xl text-[var(--mainColor)] cursor-pointer duration-500' onClick={() => {setShowBar(!showBar)}}/>)}
 
-        <Link to="/"><h1 className='md:text-3xl lg:text-4xl font-bold text-[var(--mainColor)] font-serif cursor-pointer tracking-[5px] duration-500'>NavBar</h1></Link>
+        <Link to="/"><h1 className='text-2xl md:text-3xl lg:text-4xl font-bold text-[var(--mainColor)] font-serif cursor-pointer tracking-[5px] duration-500'>NavBar</h1></Link>
 
         <div className='flex gap-2.5 items-center duration-500'>
-            <CiSearch className='md:text-xl lg:text-2xl text-[var(--mainColor)] cursor-pointer z-10 duration-500' onClick={() => {setShowSearch(!showSearch)}}/>
+            <CiSearch className='text-xl lg:text-2xl text-[var(--mainColor)] cursor-pointer z-10 duration-500' onClick={() => {setShowSearch(!showSearch)}}/>
 
-            <Link to="/login"><Button title='Login' className="lg:text-xl border border-[var(--mainColor)] text-[var(--mainColor)] px-[20px] rounded cursor-pointer hover:bg-[var(--background)] duration-600"/></Link>
+            <Link to="/login"><Button title='Login' className="hidden md:block lg:text-xl border border-[var(--mainColor)] text-[var(--mainColor)] px-[20px] rounded cursor-pointer hover:bg-[var(--background)] duration-600"/></Link>
 
-            <HiOutlineShoppingBag className='text-2xl text-[var(--mainColor)] cursor-pointer duration-500'/> 
+            <HiOutlineShoppingBag className='text-xl lg:text-2xl text-[var(--mainColor)] cursor-pointer duration-500'/> 
 
             {showSearch && <div className='absolute right-[12%] duration-500'>
                 <input type="search" placeholder='search...' className='lg:w-70 border rounded border-[var(--mainColor)] placeholder:text-[var(--mainColor)] placeholder:text-[14px] text-[14px] px-[20px] py-[3px] outline-none text-[var(--mainColor)] duration-500'/>
@@ -38,7 +38,7 @@ const NavBar = () => {
 
 
         { showBar && 
-        (<div className="absolute w-[400px] flex flex-col top-[80px] left-0 h-[calc(100vh-80px)] pt-10 tracking-[4px] pl-10 duration-500 all ease-in bg-[var(--background)]">
+        (<div className="absolute w-[400px] flex flex-col top-[70px] left-0 h-[calc(100vh-70px)] pt-10 tracking-[4px] pl-10 duration-500 all ease-in bg-[var(--background)] z-50">
             {
                 NavBar.map(({id, title, href}) => {
                     return <NavLink to={href} key={id} className="text-xl text-[var(--mainColor)] font-thin hover:bg-[var(--background)] pl-2.5 duration-600">{title}</NavLink>
