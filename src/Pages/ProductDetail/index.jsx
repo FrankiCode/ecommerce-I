10840569dc29d359d428d8bf303370c683e5b071
FrankiCode <jaceknauka@gmail.com>
2025-03-12 17:16:24 +0400

    no t=change

--- src/Pages/ProductDetail/index.jsx
+++ src/Pages/ProductDetail/index.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router'
 import NotFound from '../../Pages/NotFound'
 import Loading from '../../Components/Loading'
 import { toast } from 'react-hot-toast';
+import NavBar from '../../Components/Layout/NavBar'
 
 const ProductDetail = () => {
 
@@ -42,23 +43,26 @@ if (isLoading) {
 
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-30 w-[90%] mx-auto mt-5'>
-        <div className='grid grid-cols-[1fr_100px] gap-2.5'>
-          <img src={currentImage} alt=""  className='w-full'/>
+      <div>
+        <NavBar/>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-30 w-[90%] mx-auto mt-5'>
+            <div className='grid grid-cols-[1fr_100px] gap-2.5'>
+              <img src={currentImage} alt=""  className='w-full'/>
 
-          <div className='grid grid-cols-[100px] grid-rows-[100px_100px_100px] object-cover gap-1'>
-            {products.images?.map((image) => {
-              return <img src={image} alt=""  className='w-full h-full cursor-pointer' onClick={() => {setCurrentImage(image)}}/>
-              
-            })}</div>
+              <div className='grid grid-cols-[100px] grid-rows-[100px_100px_100px] object-cover gap-1'>
+                {products.images?.map((image) => {
+                  return <img src={image} alt=""  className='w-full h-full cursor-pointer' onClick={() => {setCurrentImage(image)}}/>
+
+                })}</div>
+            </div>
+            <div>
+                <h2 className='font-bold mb-2.5 text-[var(--productTitle)] '>{products.title}</h2>
+                <h2 className='text-[14px] mb-2.5'>{products.description}</h2>
+                <h2 className='text-[12px] md:text-[14px] text-[var(--mainColor)]'>{products.category?.name}</h2>
+                <h2 className='text-[var(--productTitle)] font-bold text-2xl'>{products.price}€</h2>
+            </div>
         </div>
-        <div>
-            <h2 className='font-bold mb-2.5 text-[var(--productTitle)] '>{products.title}</h2>
-            <h2 className='text-[14px] mb-2.5'>{products.description}</h2>
-            <h2 className='text-[12px] md:text-[14px] text-[var(--mainColor)]'>{products.category?.name}</h2>
-            <h2 className='text-[var(--productTitle)] font-bold text-2xl'>{products.price}€</h2>
-        </div>
-    </div>
+      </div>
   )
 }
 
