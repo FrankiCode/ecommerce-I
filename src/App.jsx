858efc4f097cb0593e1f623242995bf92bf84d2a
FrankiCode <jaceknauka@gmail.com>
2025-03-12 17:14:05 +0400

    nothing chnged

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/Layout/NavBar'
 import WebRoutes from './Components/WebRoutes'
+import Products from './Pages/Products';
 
-function App() {
+function App(){  
 
+  // const [searchText, setSearchText] = useState("");
+  
 
   return (
     <div className=' mx-auto'>
-      <NavBar/>
+      {/* <NavBar searchText={searchText} setSearchText={setSearchText}/> */}
       <WebRoutes/>
     </div>
   )
